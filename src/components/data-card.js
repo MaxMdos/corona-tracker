1941d104b3f15baa3430048123a7b7172c425cb3
Maximiliano Martinez <maximilianomartinez15@outlook.com>
2020-05-14 15:33:53 -0400

    added stuff

--- src/components/data-card.js
+++ src/components/data-card.js
@@ -4,13 +4,13 @@ import { Card } from '@material-ui/core';
 const DataCard = (props) => {
     return (
         <Card>
-                <h1>Confirmed Cases</h1>
+            <h1>Confirmed Cases</h1>
             <p>{props.confirmed.value}</p>
             <h1>Deaths</h1>
             <p>{props.deaths.value}</p>
             <h1>Recovered</h1>
             <p>{props.recovered.value}</p>
-            </Card>
+        </Card>
     )
 }
 
